9783c96fae1b029c93460d3da885eef1e954dac1
luis-domingues <luisdomingues117@gmail.com>
2024-12-09 15:09:05 -0300

    feat: create user model with basic properties

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,28 @@
-﻿namespace Taskly.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskly.Models;
 
 public class User
 {
-    
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength]
+    public string FullName { get; set; }
+    [Required]
+    [MinLength(6)]
+    [MaxLength(30)]
+    public string Username { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [MinLength(6)]
+    [MaxLength(30)]
+    public string Password { get; set; }
+    [Required]
+    [MaxLength(30)]
+    public string TitleJob { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
 }
