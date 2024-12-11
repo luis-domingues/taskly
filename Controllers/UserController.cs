11d30a135ed5a50ffe5ae14562be3f4d95163711
luis-domingues <luisdomingues117@gmail.com>
2024-12-11 17:05:42 -0300

    feat: add endpoints to receive the latest service requests

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.Data;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Taskly.DTOs;
 using Taskly.Exceptions;
@@ -63,6 +63,70 @@ public class UserController : ControllerBase
         catch (Exception)
         {
             return BadRequest(new { error = "Requisisão inválida." });
+        }
+    }
+
+    [HttpPut("update/{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateUserRequestDto request)
+    {
+        try
+        {
+            var updatedUser = new User
+            {
+                FullName = request.FullName,
+                Username = request.Username,
+                Password = request.Password,
+                TitleJob = request.TitleJob
+            };
+
+            var user = await _userService.UpdateUserAsync(id, updatedUser);
+            return Ok(new { message = "Informações atualizadas com sucesso." });
+        }
+        catch (UsernameAlreadyExistsException e)
+        {
+            return Conflict(new { error = e.Message });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { error = e.Message });
+        }
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        try
+        {
+            var authenticatedUserId = int.Parse(User.FindFirst("UserId")?.Value ?? "0");
+            if (id != authenticatedUserId)
+                return Unauthorized(new { error = "Acesso não permitido." });
+
+            await _userService.DeleteUserAsync(id);
+
+            return Ok(new { message = "Usuário deletado com sucesso." });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { error = e.Message });
+        }
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> SearchUsers([FromQuery] string? fullname, [FromQuery] string? username,
+        [FromQuery] string? email)
+    {
+        try
+        {
+            var users = await _userService.SearchUserAsync(fullname, username, email);
+
+            if (users.Count == 0)
+                return NotFound(new { message = "Usuário não encontrado." });
+
+            return Ok(users);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { error = e.Message });
         }
     }
 }
