3f000a2bde470212215005d0f0bd73d1060bd075
luis-domingues <luisdomingues117@gmail.com>
2024-12-11 17:03:08 -0300

    feat: improve request response to avoid exposing sensitive information

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -2,5 +2,9 @@
 
 public class UserResponseDto
 {
-    
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string TitleJob { get; set; }
 }
