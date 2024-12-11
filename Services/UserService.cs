16e33709aec309d6b8dc8f4b150247eb7229d72d
luis-domingues <luisdomingues117@gmail.com>
2024-12-11 16:52:24 -0300

    feat: implement methods of deleting, searching and updating registered user infos

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Taskly.Context;
+using Taskly.DTOs;
 using Taskly.Exceptions;
 using Taskly.Models;
 
@@ -44,5 +45,64 @@ public class UserService
             throw new IncorrectPassword("Senha incorreta.");
 
         return user;
+    }
+
+    public async Task<User> UpdateUserAsync(int userId, User updatedUser)
+    {
+        var user = await _context.Users.FindAsync(userId);
+        if (user == null)
+            throw new UsernameNull("Usuário não encontrado.");
+
+        if (updatedUser.Username != user.Username &&
+            await _context.Users.AnyAsync(u => u.Username == updatedUser.Username))
+            throw new UsernameAlreadyExistsException("Nome de usuário em uso.");
+
+        user.FullName = updatedUser.FullName;
+        user.TitleJob = updatedUser.TitleJob;
+
+        if (!string.IsNullOrEmpty(updatedUser.Username))
+            user.Username = updatedUser.Username;
+
+        if (!string.IsNullOrEmpty(updatedUser.Password))
+            user.Password = BCrypt.Net.BCrypt.HashPassword(updatedUser.Password);
+
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    public async Task DeleteUserAsync(int userId)
+    {
+        var user = await _context.Users.FindAsync(userId);
+        if (user == null)
+            throw new UserNotFoundExcepiton("Usuário não encontrado");
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<UserResponseDto>> SearchUserAsync(string? fullname, string? username, string? email)
+    {
+        var query = _context.Users.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(fullname))
+            query = query.Where(u => u.FullName.Contains(fullname));
+
+        if (!string.IsNullOrWhiteSpace(username))
+            query = query.Where(u => u.Username.Contains(username));
+
+        if (!string.IsNullOrWhiteSpace(email))
+            query = query.Where(u => u.Email.Contains(email));
+
+        return await query.Select(u => new UserResponseDto
+        {
+            Id = u.Id,
+            FullName = u.FullName,
+            Username = u.Username,
+            Email = u.Email,
+            TitleJob = u.TitleJob
+        })
+            .ToListAsync();
     }
 }
