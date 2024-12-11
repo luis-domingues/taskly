e2625136ef2d19fe369a0c554895cc47588bd51f
luis-domingues <luisdomingues117@gmail.com>
2024-12-11 17:00:17 -0300

    feat: encapsulate data necessary for the function of updating infos

--- DTOs/UpdateUserRequestDto.cs
+++ DTOs/UpdateUserRequestDto.cs
@@ -2,5 +2,8 @@
 
 public class UpdateUserRequestDto
 {
-    
+    public string FullName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string TitleJob { get; set; }
 }
